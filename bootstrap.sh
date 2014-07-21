b8fb34883846bd32f6bdf5816770188a2933af50
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-07-21 11:58:33 +0200

    add two missing envvars required for autoproj_bootstrap to not need any user input

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@ set -e
 mkdir dev
 cd dev
 wget https://raw.githubusercontent.com/rock-core/autoproj/stable/bin/autoproj_bootstrap
+
+export AUTOPROJ_OSDEPS_MODE=all
+export AUTOPROJ_IGNORE_NONEMPTY_DIR=1
 ruby autoproj_bootstrap git ../
 source env.sh
 amake --all -k
