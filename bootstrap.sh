d89ac1e50a61c7a4a354ef0ae1ca124b6eecdec6
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-07-21 15:22:20 +0200

    workaround an autoproj bug related to AUTOPROJ_OS
    AUTOPROJ_OS is basically ignored in some cases, which
    leads to a build failure on travis. Generate a dummy
    /etc/os-release until this is resolved

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,16 @@ wget https://raw.githubusercontent.com/rock-core/autoproj/stable/bin/autoproj_bo
 export AUTOPROJ_OSDEPS_MODE=all
 export AUTOPROJ_IGNORE_NONEMPTY_DIR=1
 export GEM_HOME=$PWD/.gems
+cat > os-release <<EOF
+ID=ubuntu
+ID_LIKE=ubuntu
+VERSION_ID=12.04
+EOF
+sudo mv os-release /etc
+
 gem install utilrb 
 export AUTOPROJ_OS=ubuntu,debian:12.04
 ruby autoproj_bootstrap git ../
 source env.sh
-export AUTOPROJ_OS=ubuntu,debian:12.04
 autoproj full-build
 
